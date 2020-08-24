285d04d23464fefabc8a4b8c4c0c84e99f8b6b4e
Cheng Lou <chenglou92@gmail.com>
2020-08-24 06:42:46 -0700

    FreeBSD support

--- format.py
+++ format.py
@@ -12,17 +12,19 @@ SETTINGS_PATH = 'Default.sublime-settings'
 resExt = ".res"
 resiExt = ".resi"
 platformSystem = platform.system()
-# rescript currently supports 3 platforms: darwin, linux, win32. these also
-# happen to be folder names for the location of the bsc binary. We're in
-# python, so we're gonna translate python's output of system to nodejs'
-# Why don't we just use the binary in node_modules/.bin/bsc? Because that
-# one's a nodejs wrapper, which has a startup cost. It makes it so that every
-# time we call it for e.g. formatting, the result janks a little.
+# rescript currently supports 4 platforms: darwin, linux, win32, freebsd.
+# These also happen to be folder names for the location of the bsc binary.
+# We're in python, so we're gonna translate python's output of system to
+# nodejs'. Why don't we just use the binary in node_modules/.bin/bsc? Because
+# that one's a nodejs wrapper, which has a startup cost. It makes it so that
+# every time we call it for e.g. formatting, the result janks a little.
 platformInNodeJS = "linux"
 if platformSystem == "Darwin":
   platformInNodeJS = "darwin"
 elif platformSystem == "Windows":
   platformInNodeJS = "win32"
+elif platformSystem == "FreeBSD":
+  platformInNodeJS = "freebsd"
 
 bscPartialPath = os.path.join("node_modules", "bs-platform", platformInNodeJS, "bsc.exe")
 
